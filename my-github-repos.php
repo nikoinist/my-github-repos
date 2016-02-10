65bd1dd089188126a71482eef199f7dff17a657e
Zoran <zoran.mihelcic@gmail.com>
2016-02-10 19:04:21 +0000

    init commit

---
+++ my-github-repos.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: My github repos
+ * Description: Display user repos in widget
+ * Version: 1.0
+ * Author: Zoran Mihelcic
+ * 
+ **/
+ //Exit if Accessed Diretctliy
+ if(!defined('ABSPATH')){
+     exit;
+ }
+
+//Load scripts
